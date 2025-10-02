194f9755f181843ab4ec18022595f46721755dcb
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-10-02 16:53:46 +0300

    chore: setup TailwindCSS with Vite and React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-blue-600">
+          Hello React + TypeScript + Tailwind!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
