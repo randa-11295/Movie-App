f71e35b1f7a7be13dcf828f73ede1f3ad5094d6c
randa.ibrahim <randa.ibrahim@cartologic.com>
2025-10-05 03:28:54 +0300

    style : add search input and logo

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchMovies } from "../api/movie";
 import MovieCard from "../components/cards/MovieCard";
 import { MovieType } from "../types/moviesTypes";
+import logo from "../assets/logo.png";
 
 function Movies() {
   const { data, error, isLoading } = useQuery({
@@ -23,22 +24,37 @@ function Movies() {
       </div>
     );
 
-  return (
     
-   
-        <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {data?.Search?.map((movie: MovieType) => (
-            <MovieCard
-              key={movie.imdbID}
-              title={movie.Title}
-              year={movie.Year}
-              type={movie.Type}
-              poster={movie.Poster}
-              id={movie.imdbID}
-            />
-          ))}
+  return (
+    <>
+      <div className="flex justify-between mb-10 items-center">
+        <img src={logo} alt="Logo" className="h-16 w-auto" />
+
+        <div className="md:w-1/2  flex gap-2">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="grow-1 p-2 rounded-lg bg-gray-800 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button
+            // onClick={handleClick}
+            className="p-3 bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium py-2 rounded-lg transition-colors dark:bg-blue-500 dark:hover:bg-blue-600"
+          > search</button>
         </div>
-  
+      </div>
+      <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {data?.Search?.map((movie: MovieType) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            year={movie.Year}
+            type={movie.Type}
+            poster={movie.Poster}
+            id={movie.imdbID}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
